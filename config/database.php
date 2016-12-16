6f869ef12ccb3612b02f13fb622a51e681ebf881
huaerjie <286252682@qq.com>
2016-12-16 14:38:03 +0800

    1.默认不启动mysql链接

--- config/database.php
+++ config/database.php
@@ -4,11 +4,12 @@ return [
     'database'=>[
         'master' => [
             'host' => '127.0.0.1',
-            'user' => 'keaixiaou',
+            'port' => 3306,
+            'user' => 'test',
             'password' => '123456',
             'database' => 'test',
             'asyn_max_count' => 4,
-            'start_count' => 1,
+            'start_count' => 0,
         ],
     ]
 
