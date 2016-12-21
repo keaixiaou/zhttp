75867e965dfd086038aa189c2c5bfcbfbbb899f6
huaerjie <286252682@qq.com>
2016-12-21 15:38:44 +0800

    1.文档完善

--- config/database.php
+++ config/database.php
@@ -6,7 +6,7 @@ return [
             'host' => '127.0.0.1',
             'port' => 3306,
             'user' => 'test',
-            'password' => '123456',
+            'password' => 'test',
             'database' => 'test',
             'asyn_max_count' => 4,
             'start_count' => 0,
